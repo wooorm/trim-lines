bf66ab0de163b0c64458def040b078feb414e39e
Titus Wormer <tituswormer@gmail.com>
2020-10-28 11:05:03 +0100

    Refactor code to improve bundle size

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 module.exports = trimLines
 
-var ws = /[ \t]*\n+[ \t]*/g
-var newline = '\n'
-
 function trimLines(value) {
-  return String(value).replace(ws, newline)
+  return String(value).replace(/[ \t]*\n+[ \t]*/g, '\n')
 }
