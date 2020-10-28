d22786eb575b1899b6b1e0c5e33fa72d753f752a
Titus Wormer <tituswormer@gmail.com>
2020-10-28 11:04:19 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 var test = require('tape')
 var trimLines = require('.')
 
-test('trimLines(value)', function(t) {
+test('trimLines(value)', function (t) {
   t.equal(trimLines(true), 'true', 'should coerce to string')
   t.equal(
     trimLines(' foo\t\n\n bar \n\tbaz '),
