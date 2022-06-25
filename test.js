dd40e6f8db1914a7a8e727ba7864cb1bf36b7620
Titus Wormer <tituswormer@gmail.com>
2022-06-25 20:53:57 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'tape'
 import {trimLines} from './index.js'
 
 test('trimLines(value)', function (t) {
-  // @ts-ignore coerce.
+  // @ts-expect-error coerce.
   t.equal(trimLines(true), 'true', 'should coerce to string')
   t.equal(
     trimLines(' foo\t\n\n bar \n\tbaz '),
